7280ad6bf12770b179cf6b47d180675983e0380b
xand6r <shuaibualexander@gmail.com>
2024-11-07 08:52:29 +0100

    clean ccamp repo

--- ccamp/lib/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-	transform: {
-		'^.+\\.[t|j]s$': ['babel-jest', { configFile: './babel-jest.config.js' }],
-	},
-	modulePathIgnorePatterns: ['<rootDir>/dist/'],
-	testTimeout: 300000,
-};
