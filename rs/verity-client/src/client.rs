8e250dce8d95008d27218ef49b144b4c43279579
Victor Shevtsov <victorshevtsov@gmail.com>
2024-12-04 19:36:59 +0000

    Fix the broken macros call tokio::try_join!

--- rs/verity-client/src/client.rs
+++ rs/verity-client/src/client.rs
@@ -128,7 +128,7 @@ impl VerityClient {
             timeout_cancellation_token.clone(),
         )?;
 
-        let (response, proof_msg) = tokio::try_join(
+        let (response, proof_msg) = tokio::try_join!(
             self.send_request(req, request_cancellation_token, timeout_cancellation_token),
             proof_awaiter,
         )
