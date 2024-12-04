88bb28fa4f1dcff79f8ff036fe79f228dca6763b
Victor Shevtsov <victorshevtsov@gmail.com>
2024-12-04 16:50:30 +0000

    Get the indentation back to 4 spaces

--- rs/verity-client/src/error.rs
+++ rs/verity-client/src/error.rs
@@ -4,11 +4,11 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Error, Debug)]
 pub enum Error {
-	/// There was an error running verity
-	#[error("Verity error: {0}")]
-	Verity(#[from] anyhow::Error),
+    /// There was an error running verity
+    #[error("Verity error: {0}")]
+    Verity(#[from] anyhow::Error),
 
-	/// Error from the underlying Reqwest client
-	#[error("Reqwest error: {0}")]
-	Reqwest(#[from] reqwest::Error),
+    /// Error from the underlying Reqwest client
+    #[error("Reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
 }
