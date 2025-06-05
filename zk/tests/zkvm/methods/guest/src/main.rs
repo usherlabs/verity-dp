5370240d5779118473cafd9127fc44ece1ea2619
Victor Shevtsov <victorshevtsov@gmail.com>
2025-06-05 09:54:38 +0000

    Update zk/tests/zkvm

--- zk/tests/zkvm/methods/guest/src/main.rs
+++ zk/tests/zkvm/methods/guest/src/main.rs
@@ -1,11 +1,25 @@
+use std::io::Read;
+
 use risc0_zkvm::guest::env;
-use verity_verify_tls::verify_proof;
+use verity_verify_private_transcript::{presentation::Presentation, CryptoProvider};
 
 fn main() {
-    let proof: String = env::read();
+    let mut presentation_bytes: Vec<u8> = vec![];
+    env::stdin().read_to_end(&mut presentation_bytes).unwrap();
 
-    let (recv, sent) = verify_proof(&proof).unwrap();
+    let presentation: Presentation = bincode::deserialize(&presentation_bytes).unwrap();
 
-    env::commit(&recv);
+    let mut transcript = presentation
+        .verify(&CryptoProvider::default())
+        .unwrap()
+        .transcript
+        .unwrap();
+
+    transcript.set_unauthed(b'X');
+
+    let sent = String::from_utf8(transcript.sent_unsafe().to_vec()).unwrap();
+    let received = String::from_utf8(transcript.received_unsafe().to_vec()).unwrap();
+
     env::commit(&sent);
+    env::commit(&received);
 }
