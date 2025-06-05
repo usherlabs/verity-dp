daf32e77f230f9cdccba117061cdc66934745c25
Victor Shevtsov <victorshevtsov@gmail.com>
2025-06-05 06:56:45 +0000

    Update zk/bench

--- zk/bench/precompute_benchmark/methods/guest/src/main.rs
+++ zk/bench/precompute_benchmark/methods/guest/src/main.rs
@@ -1,37 +1,34 @@
 use std::io::Read;
 
 use risc0_zkvm::guest::env;
-use serde::{Deserialize, Serialize};
-
 use verity_verify_private_transcript::{
-    encodings_precompute::{verify, verify_raw, EncodingsMapType},
-    presentation::Presentation,
+    encodings_precompute::{verify, verify_raw},
     transcript::PartialTranscript,
 };
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ZKInput {
-    presentation: Presentation,
-    encodings: Option<EncodingsMapType>,
-}
+use zk_bench_core::{ZKInput, ZKOutput};
 
 fn main() {
-    // read the input
-    let mut input_bytes: Vec<u8> = vec![];
-    env::stdin().read_to_end(&mut input_bytes).unwrap();
+    let mut zk_input_bytes: Vec<u8> = vec![];
+    env::stdin().read_to_end(&mut zk_input_bytes).unwrap();
 
     let ZKInput {
         presentation,
         encodings,
-    } = bincode::deserialize(&input_bytes).unwrap();
-    // env::log(&format!("Processing value: {:?}", presentation));
+    } = bincode::deserialize(&zk_input_bytes).unwrap();
 
-    let partial_transcript_response: PartialTranscript = if encodings.is_some() {
+    let mut partial_transcript: PartialTranscript = if encodings.is_some() {
         verify(&presentation, encodings.as_ref()).unwrap()
     } else {
         verify_raw(&presentation).unwrap()
     };
 
-    // write public output to the journal
-    env::commit(&10);
+    partial_transcript.set_unauthed(b'X');
+
+    let zk_output = ZKOutput {
+        sent: String::from_utf8(partial_transcript.sent_unsafe().to_vec()).unwrap(),
+        received: String::from_utf8(partial_transcript.received_unsafe().to_vec()).unwrap(),
+    };
+
+    let zk_output_bytes = bincode::serialize(&zk_output).unwrap();
+    env::commit(&zk_output_bytes);
 }
