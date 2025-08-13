3b1ccbd7f30be374adb799fbee550b750cb3082e
rsoury <rsoury@users.noreply.github.com>
2025-08-13 23:19:36 +0000

    deploy: 567ae802d774fbbe1dcab89020b49619f57acd7a

--- packages/verity-client/doc/trait.impl/serde/de/trait.Deserialize.js
+++ packages/verity-client/doc/trait.impl/serde/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["verity_client",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"verity_client/client/struct.NotaryInformation.html\" title=\"struct verity_client::client::NotaryInformation\">NotaryInformation</a>"]]]]);
+    var implementors = Object.fromEntries([["verity_client",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"verity_client/client/struct.NotaryInformation.html\" title=\"struct verity_client::client::NotaryInformation\">NotaryInformation</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
